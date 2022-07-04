224ee31692fcfb375cf633cafd3fa43232aff1e6
nicolas <nicolas.rossat@hotmail.fr>
2022-07-04 10:53:22 +0200

    Ajout exemple code Abstract Factory

--- code/ConsoleApp1/ConsoleApp1/Program.cs
+++ code/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,8 +1,63 @@
-﻿
+﻿using System;
 // Abstract factory design pattern
 
-Console.WriteLine("Hello, World!");
+// Classe mère catalogue abstract
+public abstract class Automobile
+{
+    protected string modele;
+    protected string couleur;
+    protected int puissance;
+    protected double espace;
 
-// Classe catalogue (mère)
+    protected Automobile(string modele, string couleur, int puissance, double espace)
+    {
+        this.modele = modele;
+        this.couleur = couleur;
+        this.puissance = puissance;
+        this.espace = espace;
+    }
 
+    public abstract void afficherCarac();
+}
 
+// Classe fille de Catalogue
+public class AutomobileElectrique : Automobile
+{
+    public AutomobileElectrique(string modele, string couleur, int puissance, double espace) : base(modele, couleur, puissance, espace)
+    {
+    }
+
+    public override void afficherCarac()
+    {
+        Console.WriteLine("Automobile éléctrique : ");
+        Console.WriteLine("Modèle : " + modele);
+        Console.WriteLine("Couleur : " + couleur);
+        Console.WriteLine("Puissance : " + puissance);
+        Console.WriteLine("Espace : " + espace);
+    }
+}
+
+// Interface Factory 
+public interface FabriqueVehicule
+{
+    Automobile creerAutomobile(string modele, string couleur, int puissance, double espace);
+
+}
+
+// Classe factory pour creer un véhicule éléctrique
+public class FabriqueVehiculeElectique : FabriqueVehicule
+{
+    public Automobile creerAutomobile(string modele, string couleur, int puissance, double espace)
+    {
+        return new AutomobileElectrique(modele, couleur, puissance, espace);
+    }
+}
+
+public class Catalogue
+{
+    static void Main(string[] args)
+    {
+        FabriqueVehicule fabrique = new FabriqueVehiculeElectique();
+        Console.WriteLine("Hello, World!");
+    }
+}
--- notes/PriseDeNote.md
+++ notes/PriseDeNote.md
@@ -1,27 +1,34 @@
 # Design patterns
 
 ## Définition
+
 Schéma d’objet qui permette de trouver une solution à un problème courant.
 
-## Pré requis 
+## Pré requis
 
 -Créer un repo Git bien documenter avec les prises de note de la semaine et les exercices.
+
 -C# / Dotnet 6.
+
 -Faire le code en anglais.
+
 -Commenter en anglais le code si possible sinon en français.
+
 -Base solide en POO.
 
 ## Objectifs
 
 -Applis console pour les exercices.
-- Pattern de construction.
--Pattern de structuration.
--Pattern de comportement.
 
+- Pattern de construction.
+
+-Pattern de structuration.
+
+-Pattern de comportement.
 
 ## Designs patterns de construction
 
-### Abstract factory 
+### Abstract factory
 
 Permet de déresponsabiliser le classe mère. Elle permet de pouvoir modifier un comportement sans modifier la classe mère.
 
@@ -34,4 +41,3 @@ Description partie de la classe scooter
 La classes FabriqueVéhiculeElectrique et FabriqueVéhiculeEssence implémente une méthode qui permet de créer un scooter en fonction de son type ScooterElectrique ou ScooterEssence.
 
 ![alt text](./img/AbstractFactory2.png)
-
